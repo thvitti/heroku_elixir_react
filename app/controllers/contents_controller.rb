a1bc0cd07b424d6c30bce240745d194924899305
thvitti <thvittix@gmail.com>
2021-04-25 10:45:44 -0300

    form content added

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,4 +5,25 @@ class ContentsController < ApplicationController
     @contents = current_user.contents
   end
 
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = current_user.contents.build(content_params)
+
+    if @content.save
+      redirect_to contents_path, notice: "Content successfully created!"
+    else
+      render :new, alert: @content.errors.full_messages.join(' ') 
+    end
+
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :description)
+  end
+
 end
